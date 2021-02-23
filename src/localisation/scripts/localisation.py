bfd58a1dfe16825e22e6f3006ced4ab30f3fd3a4
Lucas GUENEBAUD <lucas.guenebaud@gmail.com>
2021-02-23 10:30:50 +0100

    added localisation

---
+++ src/localisation/scripts/localisation.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+#>>> input : 
+#GPSFix gps_pos
+#spatialie map #TODO
+
+#>>> output :
+#LocOutput pose
+
+import rospy
+from std_msgs.msg import String
+from gps_common import GPSFix #TODO: fix import
+from carlaROSmsgs import LocOutput #TODO: make custom message LocOutput
+
+gps_pos = None
+
+def new_gps_pos(data):
+    global gps_pos
+    gps_pos = data
+
+def get_pose(pos):
+    return pos
+
+def node():
+    #init
+    rospy.init_node('localisation', anonymous=True)
+    #input
+    rospy.Subscriber('gps', GPSFix, new_gps_pos)
+    #output
+    loc_output = rospy.Publisher('loc_output', LocOutput, queue_size=10)
+
+    #loop
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        pose = get_pose(gps_pos)
+        rospy.loginfo(output)
+        loc_output.publish(pose)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        node()
+    except rospy.ROSInterruptException:
+        pass
